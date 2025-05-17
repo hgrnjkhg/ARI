617c80ecff4f58ff80f481e01d17ec71da3b9852
hgrnjkhg <205313624+hgrnjkhg@users.noreply.github.com>
2025-05-17 23:09:01 +0200

    creacio de personatge amb animacio + inici del fondo

---
+++ personal/character_body_2d.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
+const SPEED = 200
+const JUMP_VELOCITY = -400
+
+func _physics_process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	
+	# Mou a la dreta
+	if Input.is_action_pressed("dreta"):
+		velocity.x += SPEED
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	# Mou a l'esquerra
+	elif Input.is_action_pressed("esquerra"):
+		velocity.x -= SPEED
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+	# No es mou horitzontalment
+	else:
+		velocity.x = 0
+		animated_sprite.play("idle")
+	# Salt
+	if Input.is_action_pressed("salt"):
+		animated_sprite.play("jump")
+		
+	self.velocity = velocity
+	move_and_slide()
